cb5749eb8a26b2a36b02c4110cac50318b9d9026
johnpgeipel <johnpgeipel@gmail.com>
2020-09-17 16:32:50 -0500

    Background changer code

--- auto-write-text/script.js
+++ auto-write-text/script.js
@@ -1,11 +1,29 @@
-const text = "hello, my name is john!";
+// const text = "hello, my name is john!";
+// let index = 0;
+
+// writeText = () => {
+//     // document.body.innerText = text.slice(0, index);
+//     let letter = text.slice(0, index);
+//     const box = document.getElementById("box");
+//     box.textContent = letter;
+//     index++;
+
+//     index > text.length ? index = 0 : '';
+// }
+
+// setInterval(writeText, 100);
+
+const TEXT = "hello, my name is john!";
 let index = 0;
 
 writeText = () => {
-    document.body.innerText = text.slice(0, index);
+    document.body.innerText = TEXT.slice(0, index);
     index++;
 
-    index > text.length ? index = 0 : '';
+    index > TEXT.length ? index = 0 : '';
 }
 
 setInterval(writeText, 100);
+
+
+
