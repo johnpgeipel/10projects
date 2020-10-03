b02524cb7cf544e1e27a570b9d1c67b84b5b93aa
johnpgeipel <johnpgeipel@gmail.com>
2020-10-03 17:45:17 -0500

    random size generator added to ghost

--- ghost-party/script.js
+++ ghost-party/script.js
@@ -1,18 +1,26 @@
 const createGhost = () => {
-    const ghost = document.createElement('div');
-    ghost.classList.add('ghost');
+    const ghost = document.createElement('img');
+    const size = Math.random() * 80 + 80 + 'px';
 
+    let direction = Math.round(Math.random())
+    if (direction === 1) {
+        ghost.style.transform = 'translateX(-200px)';
+    } else {
+        ghost.style.transform = 'translateX(200px)';
+    };
+
+    ghost.classList.add('ghost');
+    ghost.src = './images/ghost.png';
+    ghost.style.width = size;
+    ghost.style.height = size;
     ghost.style.left = Math.random() * 100 + 'vw';
     ghost.style.animationDuration = Math.random() * 2 + 5 +'s';
-    ghost.style.fontSize = Math.random() * 8 + 2 + "rem";
-
-    ghost.innerText = '👻';
-
+    
     document.body.appendChild(ghost);
 
     setTimeout(() => {
         ghost.remove();
     }, 10000);
-}
+};
 
 setInterval(createGhost, 500);
