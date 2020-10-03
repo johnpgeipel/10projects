5abde7e8a7c5758735897a64b1473c0d8c96a659
johnpgeipel <johnpgeipel@gmail.com>
2020-10-03 18:54:01 -0500

    added ghost game

--- ghost-party/script.js
+++ ghost-party/script.js
@@ -1,26 +1,33 @@
 const createGhost = () => {
     const ghost = document.createElement('img');
-    const size = Math.random() * 80 + 80 + 'px';
+    const size = Math.random() * 50 + 80 + 'px';
 
     let direction = Math.round(Math.random())
     if (direction === 1) {
-        ghost.style.transform = 'translateX(-200px)';
+        ghost.src = './images/leftGhost.png';
+        ghost.style.transform = 'translateX(' + Math.random() * 200 + 100 + 'px)';
     } else {
-        ghost.style.transform = 'translateX(200px)';
+        ghost.src = './images/ghost.png';
+        ghost.style.transform = 'translateX(-' + Math.random() * 200 + 100 + 'px)';
     };
 
     ghost.classList.add('ghost');
-    ghost.src = './images/ghost.png';
     ghost.style.width = size;
     ghost.style.height = size;
     ghost.style.left = Math.random() * 100 + 'vw';
-    ghost.style.animationDuration = Math.random() * 2 + 5 +'s';
+    ghost.style.animationDuration = Math.random() * 2 + 10 +'s';
     
     document.body.appendChild(ghost);
 
     setTimeout(() => {
         ghost.remove();
     }, 10000);
+
+    ghost.addEventListener("click", () => {
+        ghost.remove();
+    });
 };
+
+
 
 setInterval(createGhost, 500);
