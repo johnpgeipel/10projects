6beb75535a04770d65b93e596f44bc71eb826edc
johnpgeipel <johnpgeipel@gmail.com>
2020-10-03 22:38:43 -0500

    added score and speed adjustments

--- ghost-party/script.js
+++ ghost-party/script.js
@@ -1,8 +1,12 @@
+let scoreNum = 0;
+
 const createGhost = () => {
     const ghost = document.createElement('img');
     const size = Math.random() * 50 + 80 + 'px';
 
+
     let direction = Math.round(Math.random())
+
     if (direction === 1) {
         ghost.src = './images/leftGhost.png';
         ghost.style.transform = 'translateX(' + Math.random() * 200 + 100 + 'px)';
@@ -11,21 +15,39 @@ const createGhost = () => {
         ghost.style.transform = 'translateX(-' + Math.random() * 200 + 100 + 'px)';
     };
 
+
+
     ghost.classList.add('ghost');
     ghost.style.width = size;
     ghost.style.height = size;
-    ghost.style.left = Math.random() * 100 + 'vw';
+    ghost.style.left = Math.random() * 75 + 'vw';
     ghost.style.animationDuration = Math.random() * 2 + 10 +'s';
     
     document.body.appendChild(ghost);
 
+    ghost.addEventListener("click", () => {
+        ghost.remove();
+        const score = document.getElementById("headerScore");
+        scoreNum++;
+        console.log(scoreNum);
+        score.innerHTML = "<div>score: " + scoreNum + "</div>";
+    });
+
+    if (scoreNum >= 10 && scoreNum < 20) {
+        ghost.style.animationDuration = Math.random() * 2 + 5 + 's';
+    } else if (scoreNum >= 20 && scoreNum < 30) {
+        ghost.style.animationDuration = Math.random() * 2 + 3 + 's';
+    } else if (scoreNum >= 30 && scoreNum < 40){
+        ghost.style.animationDuration = Math.random() * 2 + 2 + 's';
+    } else if (scoreNum >= 40 && scoreNum < 50) {
+        ghost.style.animationDuration = Math.random() * 2 + 1 + 's';
+    } else if (scoreNum > 50) {
+        ghost.style.animationDuration = "0.5s";
+    }
+
     setTimeout(() => {
         ghost.remove();
     }, 10000);
-
-    ghost.addEventListener("click", () => {
-        ghost.remove();
-    });
 };
 
 
